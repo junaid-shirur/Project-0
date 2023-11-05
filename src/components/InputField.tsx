84baada03b6ef504c08105a2c29bfa9799decc77
Junaid <jshirur786@gmail.com>
2023-11-05 15:29:11 +0530

    modified input field

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { Colors } from '@/theme/Variables';
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet, ViewStyle } from 'react-native';
 
 interface InputFieldProps {
   placeholder: string;
   onChangeText: () => void;
+  containerStyle: ViewStyle
 }
 
 const InputField: React.FC<InputFieldProps> = ({
   placeholder,
   onChangeText,
+  containerStyle,
   ...props
 }) => {
+  const [focus,setFocus] = useState(false)
+
+  const styles = StyleSheet.create({
+    inputContainer: {
+      backgroundColor: Colors.white,
+      width: '100%',
+      borderRadius: 6,
+      borderWidth: 1,
+      borderColor: focus? Colors.blue: Colors.white,
+      shadowColor: '#0000ff',
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.2,
+      shadowRadius: 5,
+      elevation: 2, 
+    },
+    input: {
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      fontSize: 14,
+      color: Colors.input
+    },
+  });
   return (
-    <View style={styles.inputContainer}>
+    <View style={[styles.inputContainer, containerStyle]}>
       <TextInput
         placeholder={placeholder}
-        placeholderTextColor="#000"
+        placeholderTextColor={Colors.input}
         style={styles.input}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
         onChangeText={onChangeText}
         {...props}
       />
@@ -24,25 +51,6 @@ const InputField: React.FC<InputFieldProps> = ({
   );
 };
 
-const styles = StyleSheet.create({
-  inputContainer: {
-    backgroundColor: '#fff',
-    width: '100%',
-    borderRadius: 30,
-    borderWidth: 1,
-    borderColor: '#0000ff',
-    shadowColor: '#0000ff',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    elevation: 3, // Android shadow
-  },
-  input: {
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    fontSize: 16,
-    color: '#000',
-  },
-});
+
 
 export default InputField;

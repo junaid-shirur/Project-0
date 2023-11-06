5a485d89874918c855c115260443ad84509a86c2
Junaid <jshirur786@gmail.com>
2023-11-06 15:13:16 +0530

    onBoarding screen

---
+++ src/screens/auth/Onboarding.tsx
@@ -0,0 +1,58 @@
+import Button from '@/components/Button';
+import Wrapper from '@/components/Container';
+import { Images } from '@/theme';
+import { Colors } from '@/theme/Variables';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+interface OnBoardingProps {}
+
+const OnBoarding: React.FC<OnBoardingProps> = () => {
+  return (
+    <Wrapper>
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <Image style={styles.image} source={Images().sparkles.WFH} />
+        <Text style={styles.text}>Discover Your Dream Job here</Text>
+        <Text style={[styles.text, styles.subText]}>
+          Explore all the existing job roles based on your interest and study
+          major
+        </Text>
+        <View style={styles.btns}>
+          <Button type='primary' btnText='Login' onPress={() => {}}/>
+          <Button type='secondary' btnText='Register' onPress={() => {}}/>
+        </View>
+      </View>
+    </Wrapper>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    height: 250,
+    width: 250,
+    alignItems: 'center',
+    marginTop: 60,
+  },
+  text: {
+    color: Colors.blue,
+    textAlign: 'center',
+    fontFamily: 'Poppins-Regular',
+    fontSize: 35,
+    fontWeight: '600',
+    marginTop: 40,
+  },
+  subText: {
+    fontSize: 12,
+    fontWeight: '400',
+    color: Colors.black,
+    marginTop: 0,
+    padding: 10,
+  },
+  btns:{
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    marginTop: 30
+  }
+});
+export default OnBoarding;
